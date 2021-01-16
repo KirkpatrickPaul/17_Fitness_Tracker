07bfaaaa5f33d9b833d2b802212f043a7f42464f
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-01-16 16:56:54 -0600

    resistance model is made. Also added empty README.md file and installed dependancies.

---
+++ models/resistance.js
@@ -0,0 +1,28 @@
+const mongoose = requre('mongoose');
+
+const Schema = mongoose.Schema;
+
+const cardioSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a name for your workout.'
+  },
+
+  type: {
+    type: String,
+    trim: true,
+    required: 'Please enter the type of workout.'
+  },
+
+  weight: {
+    type: Number,
+    required: 'Please enter a weight amount.'
+  },
+
+  sets: { type: Number, required: 'Please enter how many sets you did' },
+
+  reps: Number,
+
+  duration: Number
+});
